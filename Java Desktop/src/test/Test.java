2f416c2d705ec807dea1ee7ca05e3d1fd5efbdf5
Lincoln Marques <Lincoln Marques@pcgamer>
2020-11-12 18:17:01 -0300

    Teste de atualização.

--- Java Desktop/src/test/Test.java
+++ Java Desktop/src/test/Test.java
@@ -15,6 +15,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("até fim deu certo");
+		System.out.println("até fim deu certo");
+		System.out.println("até fim deu certo");
+		System.out.println("até fim deu certo");
+
 
 	}
 
